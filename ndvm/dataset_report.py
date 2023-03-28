e05e2c6f65ec5ad85112282ba9c775101c877ed5
Dominik Soukup <dosoukup@cisco.com>
2023-03-28 22:06:32 +0200

    Finish metric1

--- ndvm/dataset_report.py
+++ ndvm/dataset_report.py
@@ -7,33 +7,66 @@ import metric1
 
 import yaml
 import pandas as pd
+from pprint import pprint
 
-def parseConfig(filename):
-    pass
-    with open(filename, "r") as stream:
-        try:
-            print(yaml.safe_load(stream))
-        except yaml.YAMLError as err:
-            print(err)
+class DatasetMetrics:
 
-def printReport():
-    report = {
-        "Classes": "2",
-        "Samples": "5000:5000",
-        "Features": 24,
-        "Duplicated Flows":  "0",
-        "Redundancy": {
-            "Value": 0.23
+    def __init__(self, filename):
+        self.filename = filename
+        self.classes = None
+        self.samples = []
+        self.labels = []
+        self.features = None
+        self.duplicated = None
+        self.redundancy = None
+        self.association = None
+        self.similarity = None
+
+    def parseConfig(self):
+        with open(self.filename, "r") as stream:
+            try:
+                data = yaml.safe_load(stream)
+            except yaml.YAMLError as err:
+                print(err)
+    def evalMetrics(self):
+        df_dataset = "sample_dataset/combined-doh-http.csv"
+        df_dataset = pd.read_csv(df_dataset, delimiter=",")
+        # Basic info
+        self.classes = len(df_dataset["is_doh"].value_counts())
+        for item in df_dataset["is_doh"].value_counts():
+            self.samples.append(item) 
+        for item in df_dataset["is_doh"].value_counts().index.tolist():
+            self.labels.append(item) 
+        self.features = len(df_dataset.drop(columns=["is_doh"]).columns)
+        self.duplicated = df_dataset[df_dataset.drop(columns=["is_doh"]).duplicated()].shape[0]
+
+        # Advanced metrics
+        print("Running Redundancy Metric ...")
+        self.redundancy = metric1.Redundancy(df_dataset,"is_doh")
+
+    def getReport(self):
+        report = {
+            "Classes": self.classes,
+            "Samples": self.samples,
+            "Features": self.features,
+            "Duplicated Flows":  self.duplicated,
+            "Redundancy": self.redundancy
         }
-    }
-    print(report)
+        return report
+    #print(report)
 
 if __name__ == "__main__":
     print("Running Dataset Report Evaluation")
-    parseConfig("config.yml")
-    df_dataset = "sample_dataset/combined-doh-http.csv"
-    df_dataset = pd.read_csv(df_dataset, delimiter=",")
-    df_dataset = df_dataset.sample(frac=1).reset_index(drop=True)
-    redundancy = metric1.Redundancy(df_dataset,"is_doh")
-    print("Redudancy", redundancy)
-    printReport()
+    dm = DatasetMetrics("config.yml")
+    dm.parseConfig()
+    dm.evalMetrics()
+    report = dm.getReport()
+    pprint(report)
+
+#    parseConfig("config.yml")
+#    df_dataset = "sample_dataset/combined-doh-http.csv"
+#    df_dataset = pd.read_csv(df_dataset, delimiter=",")
+#    df_dataset = df_dataset.sample(frac=1).reset_index(drop=True)
+#    redundancy = metric1.Redundancy(df_dataset,"is_doh")
+#    print("Redudancy", redundancy)
+#    printReport()
--- ndvm/metric1.py
+++ ndvm/metric1.py
@@ -19,9 +19,6 @@ from multiprocessing import Process
 
 runs = 5 # number of iterations 
 alfa = 0.01 # Lift Value
-#metric =  f1_score
-# Set label column name in the dataset -> it must be lowercase due to FET module
-LABEL="is_doh"
 # Set if your dataset is multiclass or not
 MULTICLASS=False
 X_1 = None
@@ -31,66 +28,59 @@ clfs_set = {}
 redundancy = 0
 
 
+# Evaluate specific dataset redundancy for the specific fraction between train and test part
 def eval_dataset(X_1, y_1, frac, clfs):
     global clfs_set
     global runs
 
     tmp_results = {}
-
     name, clf = clfs
-    #for name, clf in clfs_set.items():
     l = []
     tmp_results[name] = [l.copy() for i in range(runs)]
-
-    #print(runs, len(y_1))
-    #iprint(runs)
     for i in range(0,runs):
         X_train_sub, X_test_sub, y_train_sub, y_test_sub = train_test_split(X_1, y_1, test_size=1-frac, stratify=y_1, shuffle=True)
-        #for name, clf in clfs_set.items():
         clf.fit(X_train_sub, y_train_sub)
         pred = clf.predict(X_test_sub)
         tmp_results[name][i].append(metrics.f1_score(y_test_sub,pred))
     return tmp_results
 
+# Calculate redundancy score for selected classificators
 def CalculateRedundancy(X_1, y_1, max_score, clfs):
     global runs
     global alfa
-    #global max_score
-#    global X_1
-#    global y_1
     
     limit = max_score*alfa
     low = 0.0
     high = 1.0
     mid = 0
     tmp_redundancy = 0.9
-
+    
+    # Run divide and conquer finding of dataset redundancy
     while high - low > alfa:
-        print("Testing redundancy with",high, low, tmp_redundancy)
         tmp_redundancy = (high + low) / 2
         tmp_score = eval_dataset(X_1, y_1, tmp_redundancy, clfs)
         tmp_high = []
         tmp_low = []
         name, clf = clfs
-        #for name, clf in clfs.items():
         tmp = []
+        print("Testing",name, max_score, high, low)
         for item in range(runs):
             if (max_score - tmp_score[name][item][0]) < limit:
                 tmp.append(tmp_score[name][item][0])
 
-        print("comparing:",max_score, tmp_score[name][item][0])
         if len(tmp) == runs:
             tmp_high.append(tmp_redundancy)
         else:
             tmp_low.append(tmp_redundancy)
 
-        # Check div and conq
+        # Check divide and conquer borders
         if len(tmp_high) > 0:
             high = tmp_redundancy
         else:
             low = tmp_redundancy
     return 1-tmp_redundancy
 
+# Prepare X_1 and y_1 variables from the input dataset
 def PrepareDataset(dataset, label):
     global X_1
     global y_1
@@ -102,23 +92,21 @@ def PrepareDataset(dataset, label):
     y_1 = y_1.astype('category')
     y_1 = y_1.cat.codes
 
+# Get maximal redundancy across all models
 def collect_result(result):
     global redundancy
     redundancy = max(result)
     return redundancy
 
+# Get maximal dataset score to find redudancy
 def maximal_score(result):
     global max_score
-    print("max score")
-    print(result,type(result))
     for item in result:
-        print(">>>>>>>>>>", max(list(item.values())[0]))
         tmp = max(list(item.values())[0])[0]
         if tmp > max_score:
             max_score = tmp
-    #print(result)
-    print(max_score)
 
+# Main method for computing the redundancy metric
 def Redundancy(dataset, label):
     global MULTICLASS
     global runs
@@ -132,36 +120,26 @@ def Redundancy(dataset, label):
 
     if MULTICLASS:
         clfs_set = {
-          #  "DT": DecisionTreeClassifier(criterion = "gini"),
+            "DT": DecisionTreeClassifier(criterion = "gini"),
             "RF": RandomForestClassifier(class_weight="balanced", criterion='gini'),
-          #  "XGB": XGBClassifier(objective="multi:softmax"),
+            "XGB": XGBClassifier(objective="multi:softmax"),
         }
     else:
         clfs_set = {
             "DT": DecisionTreeClassifier(),
             "RF": RandomForestClassifier(),
-            "XGB": XGBClassifier(use_label_encoder=False, eval_metric="logloss"),
+            "XGB": XGBClassifier(eval_metric="logloss"),
         }
 
+    # Find max score in parallel
     pool = mp.Pool(len(clfs_set))
-    #results = eval_dataset(X_1, y_1, 0.9, clfs)
     pool.starmap_async(eval_dataset, [(X_1, y_1, 0.9,  clfs) for clfs in clfs_set.items()], callback=maximal_score)
     pool.close()
     pool.join()
 
-    #max_score = 0
-    #for name, clf in clfs_set.items(): 
-    #    for i in range(runs):
-    #        if max_score < results[name][i][0]:
-    #            max_score = results[name][i][0]
-    #max_score = 0.9
-
     
-    #pool = mp.Pool(len(clfs_set))
-    pool = mp.Pool(1)
-    #results = [pool.apply(CalculateRedundancy, args=(X_1, y_1, clfs)) for clfs in clfs_set.items()]
-    #results = pool.starmap(CalculateRedundancy, [(X_1, y_1, clfs) for clfs in clfs_set.items()])
-    #for ii in range(runs):
+    # Find max dataset redundancy score in parallel
+    pool = mp.Pool(len(clfs_set))
     pool.starmap_async(CalculateRedundancy, [(X_1, y_1, max_score,  clfs) for clfs in clfs_set.items()], callback=collect_result)
     pool.close()
     pool.join()
